5e0c45194649682306983f8ec582d5557b4ea302
Rohith M <rohith.m068@gmail.com>
2023-07-14 18:06:07 +0530

    Adding RDS instances to RDS cluster

--- main.tf
+++ main.tf
@@ -1,3 +1,27 @@
+
+resource "aws_security_group" "sg" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
+  vpc_id = var.vpc_id
+  ingress {
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = var.sg_subnet_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = merge({
+    Name = "${var.component}-${var.env}"
+  }, var.tags)
+}
+
 resource "aws_db_subnet_group" "main" {
   name       = "main"
   subnet_ids = var.subnet_ids
@@ -17,9 +41,10 @@ resource "aws_rds_cluster" "main" {
   db_subnet_group_name = aws_db_subnet_group.main.name
   kms_key_id = var.kms_key_arn
   storage_encrypted = true
+  vpc_security_group_ids = [aws_security_group.sg.id]
 }
 
-resource "aws_rds_cluster_instance" "cluster_instances" {
+resource "aws_rds_cluster_instance" "main" {
   count              = var.instance_count
   identifier         = "${var.component}-${var.env}-instance-${count.index}"
   cluster_identifier = aws_rds_cluster.main.id
